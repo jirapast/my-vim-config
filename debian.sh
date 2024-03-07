b1d6f1ae2b9b06e3e586b5eff62df05c6631a242
Jirapast B <jiissee@gmail.com>
2024-03-07 13:44:04 +0700

    Update debian.sh
    fix path bug

--- debian.sh
+++ debian.sh
@@ -8,8 +8,8 @@ sudo apt-get update
 sudo apt-get install git 
 sudo apt-get install unzip curl tree zsh vim tmux wget
 
-cp my-vim-config/.zshrc $HOME/.zshrc
-cp my-vim-config/.vimrc $HOME/.vimrc
+cp .zshrc $HOME/.zshrc
+cp .vimrc $HOME/.vimrc
 
 chsh -s /bin/zsh
 
