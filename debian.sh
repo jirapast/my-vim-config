eeb1ff7d0e5930531ab8107d839841fdf264df53
Jirapast B <jiissee@gmail.com>
2024-03-08 18:55:35 +0700

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,11 +1,3 @@
-sudo apt-get update
-sudo apt-get install git 
-sudo apt-get install unzip curl tree zsh vim tmux wget gh
-
-cp .zshrc $HOME/.zshrc
-cp .vimrc $HOME/.vimrc
-
-chsh -s $(which zsh)
 
 
 
